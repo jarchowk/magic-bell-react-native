a7995d0cca04da50456eef9c7bd863c71fe8bfcf
jarchowk <jarchow.kurt@gmail.com>
2023-11-08 06:10:20 -0500

    Initial commit

---
+++ src/Notifications/NotificationHeader.js
@@ -0,0 +1,25 @@
+import { observer } from "@magicbell/react-headless";
+import React from "react";
+import { View, Text } from "react-native";
+
+/**
+ * Component that renders the title of the notification and its
+ * creation date.
+ */
+function NotificationHeader({ notification }) {
+  return (
+    <View>
+      <Text>
+        <Text style={{ fontWeight: "bold" }}>
+          {notification.customAttributes?.sender?.name}
+        </Text>{" "}
+        {notification.title}
+      </Text>
+      <Text style={{ color: "#939da8", fontSize: 13 }}>
+        {notification.sentAtDate.format("DD MMM, YYYY")}
+      </Text>
+    </View>
+  );
+}
+
+export default observer(NotificationHeader);
